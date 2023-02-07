faee1b95db4c69b81b5dc0d0b36a6198ff55826f
romandubinin <romandubinin94@gmail.com>
2023-02-07 19:01:50 +0100

    exception handler for controllers

---
+++ EndpointsMonitoring/WebService/src/main/java/webservice/infrastructure/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package webservice.infrastructure;
+
+import com.mysql.cj.jdbc.exceptions.MysqlDataTruncation;
+import org.hibernate.StaleObjectStateException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Map<String, Object>> handleError(NoSuchElementException e) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(StaleObjectStateException.class)
+    public ResponseEntity<Map<String, Object>> handleError(StaleObjectStateException e) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Conflict on data change operation.");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    @ExceptionHandler(IllegalAccessException.class)
+    public ResponseEntity<Map<String, Object>> handleError(IllegalAccessException e) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleError(IllegalArgumentException e) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(MysqlDataTruncation.class)
+    public ResponseEntity<Map<String, Object>> handleError(MysqlDataTruncation e) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+}
